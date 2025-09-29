56899645c22fd51232aa7dd3494bbd5a0ea7dd05
tushar <tusharlevesl@icloud.com>
2025-09-29 15:15:38 -0400

    updating the code, made it DRY from WET

--- 01practice_variables.sh
+++ 01practice_variables.sh
@@ -12,6 +12,8 @@ echo networth = $1 #rhs
 echo futurenetworth =$2
 
 
+echo Todays Date is $(date)
 
-
+Add=$((100+200))
+echo valu is ${Add}
 
---
+++ 02practice_functions.sh
@@ -0,0 +1,11 @@
+
+
+Math() {
+
+  print math function testing
+
+}
+
+Math
+Math
+pwd
