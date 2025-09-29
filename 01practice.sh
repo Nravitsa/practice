9b7c1e0ed41fe27a2720bad25780e43ca281e9b8
tushar <tusharlevesl@icloud.com>
2025-09-29 14:17:04 -0400

    Practice upload

--- 01practice.sh
+++ 01practice.sh
@@ -1,7 +1,5 @@
 
 
-input1=$1
-input2=$2
 #LHS
 echo name = ${name}
 echo lastname = ${lastname}
@@ -10,8 +8,8 @@ echo age = ${age} #Env
 echo height = ${height}
 
 
-echo networth = ${input1} #rhs
-echo futurenetworth =${input2}
+echo networth = $1 #rhs
+echo futurenetworth =$2
 
 
 
